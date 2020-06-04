7048cef1bc934a0bdae8108ea9343357433267be
sonia <sonia.lamrani@epitech.eu>
2020-06-04 11:05:36 +0200

    je suis contente

--- front/src/components/calendar.js
+++ front/src/components/calendar.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, {Component} from 'react'
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
-
 
 function Calendar() {
     const [startDate, setStartDate] =
@@ -14,5 +13,4 @@ function Calendar() {
     );
     // console.log(date)
 };
-
 export default Calendar
