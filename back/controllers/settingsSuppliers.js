eb4e8b48d28d7737a61e76f51efd067b4d31f068
Simon <simon.casteran@epitech.eu>
2020-06-04 11:04:32 +0200

    supplier test operational and supplier back corrected

--- back/controllers/settingsSuppliers.js
+++ back/controllers/settingsSuppliers.js
@@ -28,20 +28,21 @@ exports.deleteProfile = (req, res) => {
     })
 }
 
-// exports.getProfile = (req, res) => {
-//     id = getId.getId(req, res)
-//     Supplier.findById(id, function (err, supplier) {
-//         if(err) throw err;
-//         res.json(supplier)
-//     })
-// }
-
 exports.getProfile = (req, res) => {
-    console.log(req.params)
-    const id =  req.params.supplierId
+    id = getId.getId(req, res)
     console.log(id)
     Supplier.findById(id, function (err, supplier) {
         if(err) throw err;
         res.json(supplier)
     })
 }
+
+// exports.getProfile = (req, res) => {
+//     console.log(req.params)
+//     const id =  req.params.supplierId
+//     console.log(id)
+//     Supplier.findById(id, function (err, supplier) {
+//         if(err) throw err;
+//         res.json(supplier)
+//     })
+// }
