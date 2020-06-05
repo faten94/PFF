21a222271e380d72559b4dba994c21c46c033ba2
Simon <simon.casteran@epitech.eu>
2020-06-05 12:15:39 +0200

    corrected auth admin in back

--- back/middlewares/admin.js
+++ back/middlewares/admin.js
@@ -11,7 +11,10 @@ module.exports = async (req, res, next) => {
     const userId = decodedToken._id;
     console.log("token admin middlewear USER ID", userId);
     if (!await adminUsers.verifyAdmin(req, res, userId)) {
-      res.json('Warning, you are not an admin')
+      // res.json('Warning, you are not an admin')
+      res.status(401).json({
+        error: new Error('Invalid request!')
+      })
     } else {
       console.log('Admin role verified')
       next();
