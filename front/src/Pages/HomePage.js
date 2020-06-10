406651b1aab705741c856c57ac035eab6f520430
Guillaume <guillaume.forest@epitech.eu>
2020-06-10 18:47:16 +0200

    [ADD] css front homepage searchbar

--- front/src/Pages/HomePage.js
+++ front/src/Pages/HomePage.js
@@ -1,17 +1,86 @@
 import React from 'react';
 //import './style/Home.css';
 import SearchBar from '../components/search/SearchBar';
+import { Button, Card, Image, Icon, Header } from 'semantic-ui-react'
+import BabySitting from '../images/babysitting.jpg'
+import CoursParticuliers from '../images/coursparticuliers.jpg'
+import Jardinage from '../images/jardinage.jpg'
+import Informatique from '../images/informatique.jpg'
+import Menage from '../images/menage.jpg'
+import Cuisine from '../images/cuisine.jpg'
 
 
 function HomePage() {
-  return (
-    <div className="homeFormDiv">
+	return (
+		<div className="homeFormDiv" >
 
-    <SearchBar/>
+<Header  textAlign='center'  style={{color: "grey"}}  icon >
+
+     <h2> All you Need is ... Help  </h2>
+    
+     <Icon name='music' />
+
+  </Header>
+
+		<div> 
+
+		<SearchBar/>
+
+		</div>
+		<br / >
+	
+
+		<Card.Group style= {{width:'100%'}} itemsPerRow={3}>
+		<Card color='red'  >
+		<Image src={BabySitting}  />
+		<Card.Content>
+		<Card.Header textAlign='center'  >Garde d'enfants</Card.Header>  
+		</Card.Content>
+		</Card >
+		<Card color='orange'>
+		<Image src={Cuisine} />
+		<Card.Content textAlign='center'>
+		<Card.Header>Cuisine</Card.Header>  
+		</Card.Content>
+		</Card>
+		<Card color='yellow'>
+		<Image src={Jardinage} />
+		<Card.Content textAlign='center'>
+		<Card.Header>Jardinage</Card.Header>  
+		</Card.Content>
+		</Card>
+		<Card color='olive'>
+		<Image src={CoursParticuliers} />
+		<Card.Content textAlign='center'>
+		<Card.Header>Cours particuliers</Card.Header>  
+		</Card.Content>
+		</Card>
+		<Card color='green'>
+		<Image src={Informatique} />
+		<Card.Content textAlign='center'>
+		<Card.Header  >Informatique</Card.Header>  
+		</Card.Content>
+		</Card>
+		<Card color='teal'>
+		<Image src={Menage} />
+		<Card.Content textAlign='center'>
+		<Card.Header>Ménage</Card.Header>  
+		</Card.Content>
+		</Card>
+		</Card.Group>
+		<br/> <br/>
+  <Header as='h3'  textAlign='center'  block>
+
+    Un site innovant de Needelp 
+  </Header>
+
+		</div>
 
 
-    </div>
-  );
-}
 
-export default HomePage;
+
+
+		);
+	}
+
+	export default HomePage;
--- front/src/components/search/SearchBar.js
+++ front/src/components/search/SearchBar.js
@@ -1,22 +1,23 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import { Button, Icon, Input } from 'semantic-ui-react'
 
 class SearchBar extends Component{
   constructor(props){
     super(props);
     this.state = {
       service:"",
-     data: [],
-   }
-   this.DisplaySupplier = this.DisplaySupplier.bind(this)
+      data: [],
+    }
+    this.DisplaySupplier = this.DisplaySupplier.bind(this)
   }
 
-handleServiceChange = (e) => {
-  console.log(e.target.value);
-     this.setState({
-       service: e.target.value
-     })
-   }
+  handleServiceChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+     service: e.target.value
+   })
+  }
   getSupplierService = (e) =>  {
     axios.post('http://localhost:8080/service',{
 
@@ -35,27 +36,33 @@ handleServiceChange = (e) => {
     if (!data.length) return null
 
 
-    return data.map((supplier, index)=>(
-      <div key={index} className='display'>
-      <p><a href= {"http://localhost:3000/accountsupplier/" + supplier._id }>{supplier.lastname} à {supplier.city} </a></p>
-      </div>
-    ))
-  }
-
-
-  render(){
-      return (
-        <div className="search">
-          <input value={this.state.service} onChange = {this.handleServiceChange}  placeholder = " Votre recherche" />
-            <button  onClick={this.getSupplierService}> Search</button>
-              <div>
-                {this.DisplaySupplier(this.state.data)}
-              </div>
+      return data.map((supplier, index)=>(
+        <div key={index} className='display'>
+        <p><a href= {"http://localhost:3000/accountsupplier/" + supplier._id }>{supplier.lastname} à {supplier.city} </a></p>
         </div>
-    )
-  }
-}
+        ))
+      }
+
+
+      render(){
+        return (
+        <div className="search">
+        
+        <Input icon value={this.state.service} onChange = {this.handleServiceChange} placeholder='à votre service !'>
+        <input />
+        <Icon name='search' />
+        </Input>
+        
+        <Button primary  onClick={this.getSupplierService}> Chercher</Button>
+        <div>
+        {this.DisplaySupplier(this.state.data)}
+        
+        </div>
+        </div>
+        )
+      }
+    }
 
 
 
-export default SearchBar;
+    export default SearchBar;
