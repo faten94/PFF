77fa873fce0d1ce203906123fc0db9dc1ffe5e8d
Jean-Christophe Gendron <Jean-ChristopheGdn@users.noreply.github.com>
2020-06-10 12:33:38 +0200

    [Add] semantic ui react

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../images/logo.png'
 import Cookies from 'js-cookie';
+import { Menu } from 'semantic-ui-react'
 
 
 function Navbar() {
@@ -25,11 +26,25 @@ function Navbar() {
       )
     }
     return (
-      <div className="navbar" style={{backgroundColor: "#2D4F6C"}}>
-        <Link to= "/"><img className ="logo" src={Logo} alt="logo"/></Link>
-        <Link to="/registeruser" className="butt">Inscription</Link>
-        <Link to="/loginUser" className="butt">Connexion</Link>
-      </div>
+      <Menu inverted fluid>
+        <Menu.Item name='home' size="1em"> 
+          <Link to= "/"><img className ="logo" src={Logo} alt="logo" width="20%"/></Link>
+        </Menu.Item>
+        <Menu.Menu position='right'>
+        <Menu.Item name='Inscription'>
+          <Link to="/registeruser" className="butt">Inscription</Link>
+        </Menu.Item>
+      
+        <Menu.Item name='Connexion'>
+          <Link to="/loginUser" className="butt">Connexion</Link>
+        </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+      // <div className="navbar" style={{backgroundColor: "#2D4F6C"}}>
+      //   <Link to= "/"><img className ="logo" src={Logo} alt="logo"/></Link>
+      //   <Link to="/registeruser" className="butt">Inscription</Link>
+      //   <Link to="/loginUser" className="butt">Connexion</Link>
+      // </div>
       );
     }
 
