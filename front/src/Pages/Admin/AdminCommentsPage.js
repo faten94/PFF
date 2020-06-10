81559db65ef1defc1709a4dff0780f1d07ac31d6
Faten <faten.zirek@epitech.eu>
2020-06-10 20:23:56 +0200

    [ADD] CSS Admin

--- front/src/Pages/Admin/AdminCommentsPage.js
+++ front/src/Pages/Admin/AdminCommentsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from "axios";
 import Cookies from 'js-cookie';
 import { Link } from "react-router-dom";
+import { Icon, Header, Table, Button } from 'semantic-ui-react'
 
 
 class AdminCommentsPage extends Component {
@@ -12,87 +13,86 @@ class AdminCommentsPage extends Component {
             admin: 'false',
         };
     }
-    
+
     getAllComments = () => {
         const headers = { 'authorization': Cookies.get('token') }
         axios
-        .get("http://localhost:8080/admin/comments", { headers: headers })
-        .then((response) => {
-            console.log("getAllusers response", response);
-            console.log("getAllusers response.data", response.data);
-            this.setState({ comments: response.data, admin: 'true' })
-        })
-        .catch((err) => {
-            console.log(err);
-            window.location.href = "http://localhost:3000/404";
-        })
+            .get("http://localhost:8080/admin/comments", { headers: headers })
+            .then((response) => {
+                console.log("getAllusers response", response);
+                console.log("getAllusers response.data", response.data);
+                this.setState({ comments: response.data, admin: 'true' })
+            })
+            .catch((err) => {
+                console.log(err);
+                window.location.href = "http://localhost:3000/404";
+            })
     }
-    
+
     displayComments = (comments) => {
         const getAllComments = comments;
         //  console.log("json", JSON.stringify(getAllUsers));
         const commentsList = getAllComments.map((comment) => {
             return (
-                <tr key={comment.id}>
-                <td>{comment.content}</td>
-                <td>{comment.date}</td>
-                <td>{comment.note}</td>
-                <td>{comment.user}</td>
-                <td>{comment.supplier}</td>
-                <td>
-                <Link to={"/admin/comment/"+comment._id}>
-                <button name={comment._id} value={comment._id}>{comment._id}</button>
-                </Link>
-                </td>
-                </tr>
-                )
-            });
-            return (commentsList);
+                <Table.Row>
+                    <Table.Cell><Link to={"/admin/comment/" + comment._id}>{comment.content}</Link></Table.Cell>
+                    <Table.Cell><Link to={"/admin/comment/" + comment._id}>{comment.date}</Link></Table.Cell>
+                    <Table.Cell><Link to={"/admin/comment/" + comment._id}>{comment.note}</Link></Table.Cell>
+                    <Table.Cell><Link to={"/admin/comment/" + comment._id}>{comment.user}</Link></Table.Cell>
+                    <Table.Cell><Link to={"/admin/comment/" + comment._id}>{comment.supplier}</Link></Table.Cell>
+                </Table.Row >
+            )
+        });
+        return (commentsList);
+    }
+
+    async componentDidMount() {
+        await this.getAllComments();
+    }
+
+    render() {
+        if (this.state.admin === 'false') {
+            return (
+                <div></div>
+            )
         }
-        
-        async componentDidMount() {
-            await this.getAllComments();
-        }
-        
-        render() {
-            if(this.state.admin === 'false'){
-                return(
-                    <div></div>
-                    )
-                }
-                else{
-                    let comments = this.state.comments;
-                    return (
-                        <div className="container">
-                        <Link to="/admin">
+        else {
+            let comments = this.state.comments;
+            return (
+                <div className="container">
+                    <Link to="/admin">
+                        <Button style={{ marginLeft: "20px" }} circular icon='arrow left' />
+                    </Link>
+                    {/* <Link to="/admin/user/create">
                         <button>
-                        Retour
+                        Create User
                         </button>
-                        </Link>
-                        <h1 className="title">Admin</h1>
-                        
-                        <h2 className="title">CRUD Comment</h2>
-                        
-                        <table id='users'>
-                        
-                        <thead>
-                        <tr>
-                        <td>Contenu</td>
-                        <td>Date</td>
-                        <td>Note</td>
-                        <td>Id Utilisateur</td>
-                        <td>Id Fournisseur</td>
-                        <td>id</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.displayComments(comments)}
-                        </tbody>
-                        </table>
-                        </div>
-                        );
-                    }
-                };
-            }
-            
-            export default AdminCommentsPage;
+                        </Link> */}
+                    <Header as='h2' icon textAlign='center'>
+                        <Icon name='users' circular />
+                        <Header.Content>Admin - Commentaires</Header.Content>
+                    </Header>
+                    <br></br>
+                    <Table celled selectable>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Contenu</Table.HeaderCell>
+                                <Table.HeaderCell>Date</Table.HeaderCell>
+                                <Table.HeaderCell>Note</Table.HeaderCell>
+                                <Table.HeaderCell>Utilisateur</Table.HeaderCell>
+                                <Table.HeaderCell>ID Fournisseur</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body>
+                            {this.displayComments(comments)}
+                        </Table.Body>
+                    </Table>
+                </div>
+
+            );
+        }
+    };
+}
+
+export default AdminCommentsPage;
