82060cb5d82e9698edac09e675af520aae8d178f
Simon <simon.casteran@epitech.eu>
2020-06-11 17:36:50 +0200

    [EDIT]paypal currency changed to EUR

--- front/src/Pages/User/ProfileUserPage.js
+++ front/src/Pages/User/ProfileUserPage.js
@@ -59,8 +59,8 @@ class ProfileUserPage extends Component {
           <p> Votre Facture est de : {devis.price} €   </p>
           <PayPalButton
         amount={devis.price}
-        currency={EUR}
-        merchantID='PVPCLN53UJUGJ'
+        currency='EUR'
+        client-id='PVPCLN53UJUGJ'
         // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
         onSuccess={(details, data) => {
           alert("Transaction completed by " + details.payer.name.given_name);
